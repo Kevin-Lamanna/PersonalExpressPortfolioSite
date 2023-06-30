35a81ea77adb188d7abb1ac251c79cc7db09e4c6
Kevin-Lamanna <kevin.lamanna2@gmail.com>
2023-06-30 17:11:19 -0400

    updated the index controller

--- controllers/index.js
+++ controllers/index.js
@@ -9,30 +9,45 @@ Web App name: https://kevin-lamanna-express-site.herokuapp.com/
 /* GET Home page. */
 module.exports.home = function(req, res, next) {
     console.log('====> From indexController home');
-    res.render('index', { title: 'Home' });
+    res.render('index', { 
+        title: 'Home', 
+        userName: req.user ? req.user.username : ''
+    });
 };
   
   
 /* GET About page. */
 module.exports.about = function(req, res, next) {
     console.log('====> From indexController about');
-    res.render('index', { title: 'About' });
+    res.render('index', { 
+        title: 'About',
+        userName: req.user ? req.user.username : ''
+    });
 };
   
 /* GET Projects page. */
 module.exports.projects = function(req, res, next) {
     console.log('====> From indexController projects');
-    res.render('index', { title: 'Projects' });
+    res.render('index', { 
+        title: 'Projects',
+        userName: req.user ? req.user.username : ''
+    });
 };
   
 /* GET Services page. */
 module.exports.services = function(req, res, next) {
     console.log('====> From indexController services');
-    res.render('index', { title: 'Services' });
+    res.render('index', { 
+        title: 'Services',
+        userName: req.user ? req.user.username : ''
+    });
 };
   
 /* GET Contact page. */
 module.exports.contact = function(req, res, next) {
     console.log('====> From indexController contact');
-    res.render('index', { title: 'Contact' });
+    res.render('index', { 
+        title: 'Contact', 
+        userName: req.user ? req.user.username : ''
+    });
 };
